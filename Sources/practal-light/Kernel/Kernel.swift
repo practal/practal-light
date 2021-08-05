aae0fa60e1bc6afb4d582b0e34a63754340b5aaa
Steven Obua <steven@recursivemind.com>
2021-08-05 19:19:06 +0100

    KCChain gets its own file.

--- Sources/practal-light/Kernel/Kernel.swift
+++ Sources/practal-light/Kernel/Kernel.swift
@@ -113,37 +113,3 @@ public struct KernelContext {
                 
 }
 
-public struct KCChain {
-    
-    private var _contexts : [KernelContext]
-        
-    public init(_ contexts : KernelContext...) {
-        self._contexts = contexts
-    }
-    
-    public var contexts : [KernelContext] {
-        return _contexts
-    }
-    
-    @discardableResult
-    public mutating func append(_ context : KernelContext) -> Bool {
-        if let last = _contexts.first {
-            guard context.parent == last.uuid else { return false }
-            _contexts.append(context)
-            return true
-        } else {
-            _contexts = [context]
-            return true
-        }
-    }
-    
-    public mutating func squash(from : Int? = nil, to : Int? = nil) -> KernelContext? {
-        let from = from ?? 0
-        let to = to ?? _contexts.count
-        guard from < to && from >= 0 && to <= contexts.count else { return nil }
-        guard let squashed = KernelContext(squash: _contexts[from ..< to]) else { return nil }
-        _contexts[from ..< to] = [squashed]
-        return squashed
-    }
-    
-}
