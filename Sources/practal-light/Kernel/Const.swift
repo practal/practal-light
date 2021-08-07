9f5d07abd02c2e79d8be7e793b4fbfe293e57711
Steven Obua <steven@recursivemind.com>
2021-08-07 04:34:38 +0100

    Update Const.swift

--- Sources/practal-light/Kernel/Const.swift
+++ Sources/practal-light/Kernel/Const.swift
@@ -58,6 +58,6 @@ public struct Const : Hashable, CustomStringConvertible {
     public static let c_ex = mkC("ex")
     public static let c_all = mkC("all")
 
-    public static let c_Prop = Const("Practal.Prop")!
+    public static let c_Prop = mkC("Prop")
 
 }
